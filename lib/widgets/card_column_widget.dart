ed5ffb7887d59757206de0aa498669ddbee24033
Grzes Furga <grfurga@google.com>
2024-07-22 10:23:51 +0200

    Added basic drag and drop

--- lib/widgets/card_column_widget.dart
+++ lib/widgets/card_column_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import '../blocs/game.dart';
+import '../models/card.dart';
 import 'card_widget.dart';
 
 class ColumnWidget extends StatelessWidget {
@@ -13,22 +14,36 @@ class ColumnWidget extends StatelessWidget {
       value: columnCubit,
       child: BlocBuilder<ColumnCubit, ColumnState>(
         builder: (context, state) {
-          return SizedBox(
-            width: 80,
-            height: 250,
-            child: Stack(
-              children: [
-                for (var i=0; i<state.cards.length; i++)
-                  Positioned(
-                    top: i * 18,
-                    left: i * 3,
-                    child: GestureDetector(
-                      onTap: () => context.read<ColumnCubit>().flipCard(i),
-                      child: CardWidget(state.cards[i]),
-                    ),
-                  ),
-              ],
-            ),
+          return DragTarget<PlayingCard>(
+            onAcceptWithDetails: (details) {
+              // Handle card drop logic
+              print("drop: ${details}");
+              //context.read<ColumnCubit>().onCardDropped(draggedCard);
+            },
+            builder: (context, candidateData, rejectedData) {
+              return SizedBox(
+                width: 80,
+                height: 250,
+                child: Stack(
+                  children: [
+                    for (var i = 0; i < state.cards.length; i++)
+                      Positioned(
+                        top: i * 18,
+                        left: i * 3,
+                        child: Draggable<PlayingCard>(
+                          data: state.cards[i],
+                          feedback: CardWidget(state.cards[i]),
+                          childWhenDragging: SizedBox.shrink(),
+                          child: GestureDetector(
+                            onTap: () => context.read<ColumnCubit>().flipCard(i),
+                            child: CardWidget(state.cards[i]),
+                          ),
+                        ),
+                      ),
+                  ],
+                ),
+              );
+            },
           );
         },
       ),
