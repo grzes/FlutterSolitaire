720d722f99fbb16a19cd14d7ea90a8ba560f533e
Grzes Furga <grfurga@google.com>
2024-07-24 18:52:31 +0200

    Wrap in Center

--- lib/screens.dart
+++ lib/screens.dart
@@ -12,75 +12,77 @@ class GameScreen extends StatelessWidget {
     OverlayEntry? overlayEntry;
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.primary,
-      body: Column(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        children: [
-          BlocBuilder<GameWon, bool>(
-            builder: (context, gameWonState) {
+      body: Center(
+        child: Column(
+          // Center is a layout widget. It takes a single child and positions it
+          // in the middle of the parent.
+          children: [
+            BlocBuilder<GameWon, bool>(
+              builder: (context, gameWonState) {
 
-              overlayEntry?.remove();
-              overlayEntry = null;
+                overlayEntry?.remove();
+                overlayEntry = null;
 
-              if (context.read<GameCubit>().state.founds.gameIsWon) {
-                Future.delayed(Duration(microseconds: 1), () {
-                  overlayEntry = WinScreen();
-                  Overlay.of(context).insert(overlayEntry!);
-                });
+                if (context.read<GameCubit>().state.founds.gameIsWon) {
+                  Future.delayed(Duration(microseconds: 1), () {
+                    overlayEntry = WinScreen();
+                    Overlay.of(context).insert(overlayEntry!);
+                  });
+                }
+                return SizedBox.shrink();
               }
-              return SizedBox.shrink();
-            }
-          ),
-          BlocBuilder<GameCubit, GameState>(
-            builder: (context, gameState) {
-              return Column(
-                children: [
-                  Container(
-                    width: 7*80,
-                    child: Row(
+            ),
+            BlocBuilder<GameCubit, GameState>(
+              builder: (context, gameState) {
+                return Column(
+                  children: [
+                    Container(
+                      width: 7*80,
+                      child: Row(
+                        children: [
+                          (gameState.canAutoPlay) ?
+                          GestureDetector(
+                            onTap: () {
+                              final autoplay = context.read<AutoPlayCubit>();
+                              final game = context.read<GameCubit>();
+                              autoplay.startAutoPlay(() => game.solveMove());
+                            },
+                            child: const CardFrame(
+                              child: Icon(Icons.play_arrow_rounded, color: Colors.white)
+                            )
+                          ) :
+                          BlocProvider.value(
+                            value: gameState.deck,
+                            child: DeckWidget(),
+                          ),
+                          Spacer(),
+                          BlocProvider.value(
+                            value: gameState.founds,
+                            child: FoundationWidget(),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Row(
+                      textDirection: TextDirection.rtl,
+                      mainAxisSize: MainAxisSize.min,
                       children: [
-                        (gameState.canAutoPlay) ?
-                        GestureDetector(
-                          onTap: () {
-                            final autoplay = context.read<AutoPlayCubit>();
-                            final game = context.read<GameCubit>();
-                            autoplay.startAutoPlay(() => game.solveMove());
-                          },
-                          child: const CardFrame(
-                            child: Icon(Icons.play_arrow_rounded, color: Colors.white)
+                        for (var column in gameState.columnCubits.reversed)
+                          Padding(
+                            padding: const EdgeInsets.all(1), // don't need that because the columns are staggered anyway
+                            child: BlocProvider.value(
+                              value: column,
+                              child: ColumnWidget()
+                            ),
                           )
-                        ) :
-                        BlocProvider.value(
-                          value: gameState.deck,
-                          child: DeckWidget(),
-                        ),
-                        Spacer(),
-                        BlocProvider.value(
-                          value: gameState.founds,
-                          child: FoundationWidget(),
-                        ),
                       ],
                     ),
-                  ),
-                  Row(
-                    textDirection: TextDirection.rtl,
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      for (var column in gameState.columnCubits.reversed)
-                        Padding(
-                          padding: const EdgeInsets.all(1), // don't need that because the columns are staggered anyway
-                          child: BlocProvider.value(
-                            value: column,
-                            child: ColumnWidget()
-                          ),
-                        )
-                    ],
-                  ),
-                ],
-              );
-            }
-          ),
-      ]),
+                  ],
+                );
+              }
+            ),
+        ]),
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           overlayEntry?.remove();
